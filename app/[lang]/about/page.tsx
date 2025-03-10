7035c3ef686d65491e1a3fc188edc6d4451c0303
Beka <bekamakharoblishvili@gmail.com>
2025-03-10 12:15:31 +0400

    fix

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -2,19 +2,27 @@ import { Authors, allAuthors } from 'contentlayer/generated'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
-// import { genPageMetadata } from 'app/seo'
+import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
-export default function Page() {
+interface PageProps {
+  params: {
+    lang: string
+  }
+}
+
+export default async function AboutPage({ params }: PageProps) {
+  const { lang } = params
+
   const author = allAuthors.find((p) => p.slug === 'default') as Authors
   const mainContent = coreContent(author)
 
   return (
-    <>
+    <div>
       {/* <AuthorLayout content={mainContent}>
         <MDXLayoutRenderer code={author.body.code} />
       </AuthorLayout> */}
-    </>
+    </div>
   )
 }
